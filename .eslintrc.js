4cdbf7c20bcd58ea322c5df823aa337cef8a63a1
denson <2671475697@qq.com>
2021-04-18 15:21:41 +0800

    modify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,50 @@
 module.exports = {
   root: true,
+  // 环境
   env: {
     browser: true,
     node: true,
     commonjs: true,
     es6: true
   },
+  // 解析器
   parser: '@typescript-eslint/parser',
   // vue
   // extends: ['plugin:vue/essential', '@vue/airbnb'],
   // react + ts
   // extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  // 插件
   // plugins: ['react', '@typescript-eslint'],
   // node + ts
   // 需安装@typescript-eslint/parser, @typescript-eslint/eslint-plugin, prettier, eslint-plugin-prettier, eslint-config-prettier, eslint
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  // extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+  // 配置标准的js风格 extends: 'standard',
+  extends: [
+    "eslint:recommended"
+  ],
+  // 编码规范
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly'
   },
+  // 解析器选项
   parserOptions: {
-    parser: 'babel-eslint'
-  },
+    // 指定使用额外的语言特性
+    ecmaFeatures: {
+      destructuring: true,
+      // 允许在全局作用使用return
+      globalReturn: true,
+      // 启用严格模式（strict mode） >= ECMAScript 5版本；
+      impliedStrict: true,
+      // 启用JSX
+      jsx: true,
+      modules: true,
+    },
   /**
-   * "off" 或 0 - Close the rules
-   * "warn" 或 1 - Open the rules，level：warn (Does not exit App),
-   * "error" 或 2 - Open the rules，level：error (Exit App)
+   * 配置检查规则
+   * "off" 或 0 - Close the rules(关闭规则)
+   * "warn" 或 1 - Open the rules, level：warn (开启规则, warn)
+   * "error" 或 2 - Open the rules, level：error (开启规则, erorr)
    */
   rules: {
     'prettier/prettier': [
@@ -37,42 +56,112 @@ module.exports = {
     ///////////////////
     //  javascript   //
     ///////////////////
-    'class-methods-use-this': 'off',
-    'no-param-reassign': 'off',
-    'no-unused-vars': 'off',
+    // 语句强制分号结尾
+    semi: [2, 'always'],
+    // 禁用 分号
+    // semi: [2, 'never'],
+    // 关闭强制使用驼峰法命名
+    camelcase: 0,
+    // 强制使用单引号
+    quotes: [2, 'single'],
+    // 强制缩进2个空格
+    indent: [2, 2, { SwitchCase: 1 }],
+    'block-spacing': 2,
+    // Controls the Spaces before and after the comma(控制逗号前后的空格)
     'comma-spacing': [2, { before: false, after: true }],
-    // "comma-dangle": [2, "never"],
-    camelcase: 'off',
-    // 'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
-
+    // 对象数组最后一个不带逗号
+    'comma-dangle': [2, 'never'],
+    // 使用windows的换行
+    'linebreak-style': [0, 'error', 'windows'],
+    // Use === instead of ==
+    eqeqeq: [2, 'allow-null'],
+    'class-methods-use-this': 0,
+    // 强制单行代码最大长度
+    'max-len': [1, 200],
+    // 函数最多只能有5个参数
+    'max-params': [0, 5],
+    // 注释风格有空格
+    'spaced-comment': 2,
+    // 必须使用 if(){} 中的{}
+    curly: [2, 'all'],
+    'no-param-reassign': 0,
+    'no-unused-vars': 0,
+    // 关闭不能有未定义的变量
+    'no-undef': 0,
+    // 禁止使用alert
+    'no-alert': 2,
+    'no-var': 2,
+    'no-debugger': 0,
+    'no-restricted-syntax': 0,
+    // 禁用 ++
+    'no-plusplus': 0,
+    // 禁用代码结束后,末尾带空格
+    'no-trailing-spaces': 0,
+    // 声明为函数参数的变量可能会引起误解
+    'no-param-reassign': 0,
+    // return 语句中不能有赋值表达式
+    'no-return-assign': 0,
+    // 允许在表达式中使用三元运算符，类似于短路评估
+    'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }],
+    // 允许修改只读全局变量
+    'no-global-assign': 0,
+    // 允许指定的标识符具有悬挂下划线
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    // 禁止 外部作用域中的变量与它所包含的作用域中的变量或参数同名
+    'no-shadow': 0,
+    // 代码空行 数量
+    'no-multiple-empty-lines': ['error'],
+    // Do not mix tabs and Spaces(禁止混用tab和空格)
+    'no-mixed-spaces-and-tabs': 2,
+    // 禁止修改const声明的变量
+    'no-const-assign': 2,
+    // 在创建对象字面量时不允许键重复
+    'no-dupe-keys': 2,
+    'no-duplicate-case': 2,
+    // Function arguments cannot be repeated(函数参数不能重复)
+    'no-dupe-args': 2,
+    // The contents of a block statement cannot be empty(块语句中的内容不能为空)
+    'no-empty': 2,
+    'no-func-assign': 2,
+    // Duplicate declarations of variables are prohibited(禁止重复声明变量)
+    'no-redeclare': 2,
+    // 函数调用时 函数名与()之间不能有空格
+    'no-spaced-func': 2,
+    // 禁止重复模块导入
+    'no-duplicate-imports': 2,
     'no-console': 0,
     'no-debugger': 0,
-    semi: ['error', 'never'], // 禁用 分号
-    'no-multiple-empty-lines': ['error'], // 代码空行 数量
-    'linebreak-style': [0, 'error', 'windows'], // 使用windows的换行
-    'comma-dangle': [2, 'never'], //  对象数组最后一个不带逗号
-    'no-trailing-spaces': 0, // 禁用 校验代码末尾带空格
-    'import/no-dynamic-require': 0, // 禁用 动态require
+    // 禁止catch子句参数与外部作用域变量同名
+    'no-catch-shadow': 2,
+    // 禁止给类赋值
+    'no-class-assign': 2,
+    // 块语句中的内容不能为空
+    'no-empty': 2,
+    // 禁止使用eval
+    'no-eval': 2,
+    // 禁止对null使用==或!=运算符
+    'no-eq-null': 2,
+    // 禁止多余的冒号
+    'no-extra-semi': 2,
+    // 禁止比较时使用NaN，只能用isNaN()
+    'use-isnan': 2,
+    // 禁用 动态require
+    'import/no-dynamic-require': 0,
     'import/no-unresolved': 0,
-    'no-param-reassign': 0, // 声明为函数参数的变量可能会引起误解
-    'max-len': ['error', 120], // 单行代码最大长度
-    'guard-for-in': 0, // 禁用 禁用for in 循环
-    'no-shadow': 0, // 禁用  禁止页面内相容参数名
-    'object-shorthand': 0, // 禁用 禁止对象内使用带引号字符串
-    'no-restricted-syntax': 0,
-    'no-plusplus': 0, // 禁用 ++
-    'consistent-return': 0, // 关闭箭头函数必须要return
-    'no-return-assign': 0, // return 语句中不能有赋值表达式
-    'global-require': 0, // 关闭禁止使用requrie
-    'prefer-promise-reject-errors': 0, // 这条规则旨在确保承诺只被Error对象拒绝。
-    'import/extensions': 'off', // 禁用文件名详细文件类型后缀
+    // 禁用文件名详细文件类型后缀
+    'import/extensions': 'off',
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'arrow-parens': ['error', 'as-needed'], // 箭头函数参数括号，可选 always：（默认）在所有情况下都需要参数；as-needed：当只有一个参数时允许省略参数
-    'no-undef': 0, // 关闭显式声明全局变量的要求
-    'class-methods-use-this': 0,
-    'no-underscore-dangle': ['error', { allow: ['_id'] }], // 允许指定的标识符具有悬挂下划线
-    camelcase: 0, // 关闭使用骆驼拼写法
-    'no-global-assign': 0, // 允许修改只读全局变量,
+    // 禁用for in 循环
+    'guard-for-in': 1,
+    // 关闭箭头函数必须要return
+    'consistent-return': 0,
+    // 关闭禁止使用requrie
+    'global-require': 0,
+    // 这条规则旨在确保承诺只被Error对象拒绝
+    'prefer-promise-reject-errors': 0,
+    // 箭头函数参数括号，可选 always：（默认）在所有情况下都需要参数；as-needed：当只有一个参数时允许省略参数
+    'arrow-parens': ['error', 'as-needed'],
+    // 函数定义时括号前面要有空格
     'space-before-function-paren': [
       'error',
       {
@@ -90,41 +179,7 @@ module.exports = {
         }
       }
     ],
-    'no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true }], // 允许在表达式中使用三元运算符，类似于短路评估
-    'block-spacing': 2,
-    quotes: [2, 'single'],
-    // Forces 2 Spaces to be indented
-    indent: [2, 2, { SwitchCase: 1 }],
-    camelcase: 0,
-    'no-var': 2,
-    'no-unused-vars': 0,
-    'no-debugger': 0,
-    // A semicolon is required at the end of a statement
-    semi: 2,
-    'spaced-comment': 2,
-    'no-const-assign': 2,
-    // 在创建对象字面量时不允许键重复
-    'no-dupe-keys': 2,
-    'no-duplicate-case': 2,
-    // Function arguments cannot be repeated(函数参数不能重复)
-    'no-dupe-args': 2,
-    // The contents of a block statement cannot be empty(块语句中的内容不能为空)
-    'no-empty': 2,
-    'no-func-assign': 2,
-    // Duplicate declarations of variables are prohibited(禁止重复声明变量)
-    'no-redeclare': 2,
-    'no-spaced-func': 0,
-    curly: [2, 'all'],
-    // Controls the Spaces before and after the comma(控制逗号前后的空格)
-    'comma-spacing': [
-      2,
-      {
-        before: false,
-        after: true
-      }
-    ],
-    // Do not mix tabs and Spaces(禁止混用tab和空格)
-    'no-mixed-spaces-and-tabs': 2,
+    // 对象字面量中冒号的前后空格
     'key-spacing': [
       2,
       {
@@ -132,21 +187,17 @@ module.exports = {
         afterColon: true
       }
     ],
-    'linebreak-style': 0,
+    //行前|行后备注
     'lines-around-comment': [
       1,
       {
         beforeBlockComment: true
       }
     ],
-    // Performs consistent spacing within the braces of the object text(对象文字的大括号内执行一致的间距)
-    'object-curly-spacing': [1, 'always'],
-    // Use === instead of ==
-    eqeqeq: [2, 'allow-null'],
-    // Forces the maximum length of a row(强制一行的最大长度)
-    'max-len': [1, 200],
-    // Duplicate module imports are not allowed(禁止重复模块导入)
-    'no-duplicate-imports': 2,
+    // 大括号内是否允许不必要的空格
+    'object-curly-spacing': [0, 'never'],
+    // 强制对象字面量缩写语法
+    'object-shorthand': 1,
     ///////////////////
     //  typescript   //
     ///////////////////
